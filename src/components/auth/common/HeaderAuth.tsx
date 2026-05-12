23f07316acd0823bcef1eb0f41504c6aaf72569d
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-05-12 11:01:55 -0300

    refactor: update layout and footer components for improved structure and performance

--- src/components/auth/common/HeaderAuth.tsx
+++ src/components/auth/common/HeaderAuth.tsx
@@ -7,9 +7,9 @@ import ModeToggle from "@/components/theme/mode-toggle";
 export default function HeaderAuth() {
   return (
     <header className="bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur-sm">
-      <div className="mx-auto flex h-14 max-w-5xl items-center justify-between px-6 sm:h-16">
+      <div className="mx-auto flex h-20 max-w-5xl items-center justify-between px-6 sm:h-24">
         <Link href="/" aria-label="Home">
-          <Logo />
+          <Logo className="mt-0" />
         </Link>
 
         <ModeToggle />
